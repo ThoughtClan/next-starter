9d49c9fba4dfb8baf0ce70e0aad55fb3ed386974
Yogeshwar Venkatesh <yogeshwar@thoughtclan.com>
2022-10-11 11:32:06 +0530

    Add support for JS obfuscation use javascript-obfuscator

--- next-pwa/next.config.js
+++ next-pwa/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextPWA = require("next-pwa");
+const WebpackObfuscator = require("webpack-obfuscator");
 const runtimeCaching = require("next-pwa/cache");
 const { i18n } = require('./next-i18next.config');
+
+const enableObfuscation = process.env.USE_JS_OBFUSCATION === "1";
+
+console.log("obfuscation flag is", enableObfuscation);
 
 const withPWA = nextPWA({
   dest: "public",
@@ -15,4 +20,22 @@ module.exports = withPWA({
   swcMinify: true,
   output: "standalone",
   i18n,
+  webpack: (config, { defaultLoaders, isServer }) => {
+    if (enableObfuscation && !isServer)
+      config.plugins.push(new WebpackObfuscator({
+        compact: true,
+        identifierNamesGenerator: 'mangled',
+        selfDefending: true,
+        stringArray: true,
+        rotateStringArray: true,
+        shuffleStringArray: true,
+        stringArrayEncoding: [],
+        stringArrayThreshold: 0.8,
+        splitStrings: true,
+        splitStringsChunkLength: 6,
+        transformObjectKeys: true,
+      }));
+
+    return config;
+  }
 });
