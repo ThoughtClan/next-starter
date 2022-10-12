0ac8d31a9fb0d15d5984870cfdb63361ae6ef253
Yogeshwar Venkatesh <yogeshwar@thoughtclan.com>
2022-10-12 10:39:38 +0530

    Add CSS classname hashing to NextJS template

--- next-pwa/next.config.js
+++ next-pwa/next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextPWA = require("next-pwa");
+const path = require('path');
+const loaderUtils = require('loader-utils');
 const WebpackObfuscator = require("webpack-obfuscator");
 const runtimeCaching = require("next-pwa/cache");
 const { i18n } = require('./next-i18next.config');
@@ -7,6 +9,21 @@ const { i18n } = require('./next-i18next.config');
 const enableObfuscation = process.env.USE_JS_OBFUSCATION === "1";
 
 console.log("obfuscation flag is", enableObfuscation);
+
+// based on https://github.com/vercel/next.js/blob/0af3b526408bae26d6b3f8cab75c4229998bf7cb/packages/next/build/webpack/config/blocks/css/loaders/getCssModuleLocalIdent.ts
+const hashOnlyIdent = (context, _, exportName) =>
+  loaderUtils
+    .getHashDigest(
+      Buffer.from(
+        `filePath:${path
+          .relative(context.rootContext, context.resourcePath)
+          .replace(/\\+/g, '/')}#className:${exportName}`,
+      ),
+      'md4',
+      'base64',
+      6,
+    )
+    .replace(/^(-?\d|--)/, '_$1');
 
 const withPWA = nextPWA({
   dest: "public",
@@ -20,7 +37,7 @@ module.exports = withPWA({
   swcMinify: true,
   output: "standalone",
   i18n,
-  webpack: (config, { defaultLoaders, isServer }) => {
+  webpack: (config, { defaultLoaders, isServer, dev }) => {
     if (enableObfuscation && !isServer)
       config.plugins.push(new WebpackObfuscator({
         compact: true,
@@ -35,6 +52,25 @@ module.exports = withPWA({
         splitStringsChunkLength: 6,
         transformObjectKeys: true,
       }));
+
+    const rules = config.module.rules
+      .find((rule) => typeof rule.oneOf === 'object')
+      .oneOf.filter((rule) => Array.isArray(rule.use));
+
+    if (!dev)
+      rules.forEach((rule) => {
+        rule.use.forEach((moduleLoader) => {
+          if (
+            moduleLoader.loader?.includes('css-loader') &&
+            !moduleLoader.loader?.includes('postcss-loader')
+          )
+            moduleLoader.options.modules.getLocalIdent = hashOnlyIdent;
+
+          // earlier below statements were sufficient:
+          // delete moduleLoader.options.modules.getLocalIdent;
+          // moduleLoader.options.modules.localIdentName = '[hash:base64:6]';
+        });
+      });
 
     return config;
   }
